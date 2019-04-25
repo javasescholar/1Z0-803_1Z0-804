95290999baf7f464d713b01549e36b91ad6d5beb
gkadnar <goran.kadnar@adcon.hr>
2019-04-25 20:42:08 +0200

    Legal identifiers

---
+++ src/hr/gkadnar/ch01/identifiers/IllegalIdentifiers.java
@@ -0,0 +1,11 @@
+package hr.gkadnar.ch01.identifiers;
+
+public class IllegalIdentifiers {
+
+	int _a;
+	int $c;
+	int ___________;
+	int ___________123_psodjf;
+	int _$;
+	int asd_ewfdf_sdafddsf_asdfsdf_12321daswed_asdf13dwedfew_fdsaf32r;
+}
---
+++ src/hr/gkadnar/ch01/identifiers/LegalIdentifiers.java
@@ -0,0 +1,10 @@
+package hr.gkadnar.ch01.identifiers;
+
+public class LegalIdentifiers {
+
+	int :b;		// identifier cannot start with :
+	int -d;		// identifier cannot start with -
+	int e#1;	// identifier cannot contain # character
+	int .f;		// identifier cannot start with .
+	int 7g;		// identifier cannot start with a number
+}
