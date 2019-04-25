598a05bc953def9d48875ca8ae4de8d9f385ffcb
gkadnar <goran.kadnar@adcon.hr>
2019-04-25 20:45:38 +0200

    legal identifiers examples

--- src/hr/gkadnar/ch01/ExamplesRunner.java
+++ src/hr/gkadnar/ch01/ExamplesRunner.java
@@ -1,12 +1,10 @@
 package hr.gkadnar.ch01;
 
 public class ExamplesRunner {
-
 	public static void main(String[] args) {
 		System.out.println("Examples explanation");
 		
 		System.out.println("Examples of legal identifiers : hr.gkadnar.ch01.identifiers.LegalIdentifiers.java");
 		System.out.println("Examples of illegal identifiers : hr.gkadnar.ch01.identifiers.IllegalIdentifiers.java");
 	}
-
 }
