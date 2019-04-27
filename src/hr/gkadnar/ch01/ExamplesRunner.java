ce86bc6b5cb869c2139759fc41437e0571ef4ba5
gkadnar <goran.kadnar@adcon.hr>
2019-04-27 08:54:08 +0200

    executable main examples

--- src/hr/gkadnar/ch01/ExamplesRunner.java
+++ src/hr/gkadnar/ch01/ExamplesRunner.java
@@ -4,7 +4,13 @@ public class ExamplesRunner {
 	public static void main(String[] args) {
 		System.out.println("Examples explanation");
 		
+		// identifiers
 		System.out.println("Examples of legal identifiers : hr.gkadnar.ch01.identifiers.LegalIdentifiers.java");
 		System.out.println("Examples of illegal identifiers : hr.gkadnar.ch01.identifiers.IllegalIdentifiers.java");
+		
+		//Using public static void main(String[] args)
+		System.out.println("Legal main methods : hr.gkadnar.ch01.executableMain.ExecutableMain1.java");
+		System.out.println("Legal main methods : hr.gkadnar.ch01.executableMain.ExecutableMain2.java");
+		System.out.println("Legal main methods : hr.gkadnar.ch01.executableMain.ExecutableMain3.java");
 	}
 }
